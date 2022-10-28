cb590fa748fc5a6c66612a9f81f76e4e55b486b5
Suraj Mishra <mishrasur7@gmail.com>
2022-10-28 22:07:20 +0300

    commented code removed from App.js

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -37,53 +37,6 @@ const Part = ({parts}) => {
 }
 
 const App = () => {
-  /*
-  commented code for step2
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <Header course = {course}/>
-      <Content 
-        parts ={{part1: part1, part2: part2, part3: part3}}
-        exercises={{exercises1: exercises1, exercises2:exercises2, exercises3:exercises3}}
-        />
-      <Total total = {exercises1 + exercises2 + exercises3}/>
-    </div>
-  )
-
-  commented code for step3
-
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  return (
-    <div>
-      <h2>{course}</h2>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-    </div>
-  )
-  */
   const course = {
     name: 'Half Stack application development',
     parts: [
