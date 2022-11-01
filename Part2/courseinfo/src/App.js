5c7de226b7e9241b4b17dae10a86e23418b1d49f
Suraj Mishra <mishrasur7@gmail.com>
2022-11-02 00:26:13 +0200

    line break added

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -10,6 +10,7 @@ const Course = ({courses}) => {
           }
           <b>total of {course.parts.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)} exercises
           </b>
+          <br />
         </div>
        )
       })}
