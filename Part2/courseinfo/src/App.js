016bc2265aacb7528b406bd4b4984c13674f3d16
Suraj Mishra <mishrasur7@gmail.com>
2022-11-02 00:30:30 +0200

    Course component declared as separate module

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -1,22 +1,4 @@
-
-const Course = ({courses}) => {
-  return (
-    <>
-      {courses.map(course => {
-       return (
-        <div key={course.id}>
-          <h2>{course.name}</h2>
-          {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)
-          }
-          <b>total of {course.parts.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)} exercises
-          </b>
-          <br />
-        </div>
-       )
-      })}
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
