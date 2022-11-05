a5729034a651b11e0c1fda808997ca229522d241
Suraj Mishra <mishrasur7@gmail.com>
2022-11-05 15:05:02 +0200

    weather moved to components folder

--- Part2/countries/src/components/CountryInfo.js
+++ Part2/countries/src/components/CountryInfo.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-import Weather from '../Weather'
+import Weather from './Weather'
 
 const CountryInfo = ({country, filteredCountriesLength}) => {
 
