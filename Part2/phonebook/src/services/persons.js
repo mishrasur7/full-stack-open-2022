4f5ab6872410c82afb4a575346fce6850264da44
Suraj Mishra <mishrasur7@gmail.com>
2022-11-07 01:06:08 +0200

    update method added to person service module

--- Part2/phonebook/src/services/persons.js
+++ Part2/phonebook/src/services/persons.js
@@ -16,4 +16,9 @@ const deletePerson = id => {
     return axios.delete(`${baseurl}/${id}`)
 }
 
-export default {getAll, create, deletePerson}
+const update = (id, newObject) => {
+    const request = axios.put(`${baseurl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, deletePerson, update}
