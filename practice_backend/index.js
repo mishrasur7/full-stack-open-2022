266b3cc8b3f5ec5fac8b2822c920f67b1cd0cf5a
Suraj Mishra <mishrasur7@gmail.com>
2022-11-08 22:35:25 +0200

    backend testing with node and express done

--- practice_backend/index.js
+++ practice_backend/index.js
@@ -23,6 +23,8 @@ let notes = [
   ]
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
     response.send('Hello suraj')
 })
@@ -30,9 +32,57 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request,response) => {
     response.json(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => {
+        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+        return note.id === id
+    })
+
+    note
+    ? response.json(note)
+    : (response.statusMessage = `Note with given id ${id} not!`, response.status(404).end())
+  })
+
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(note => note.id))
+        : 0
+    return maxId + 1
+  }
+
+  app.post('/api/notes', (request, response) => {
+    console.log(request.headers)
+    const body = request.body
+
+    if(!body.content){
+        return response.status(400).json({error: "content missing"})
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    console.log(note)
+    notes = notes.concat(note)
+    response.json(note)
+  })
+
 // const app = http.createServer((request, response) => {
-//     response.writeHead(200, {'Content_Type': 'application/json'})
-//     response.end(JSON.stringify(notes))
+//     response.writeHead(200, { 'Content-Type': 'text/plain' })
+//     response.end('Hello World')
 // })
 
 const PORT = 3001
