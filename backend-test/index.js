f65696148684ad49778f3ac73b527adc3be44435
Suraj Mishra <mishrasur7@gmail.com>
2022-11-08 12:03:45 +0200

    server setup done , type set to module in package.json

--- backend-test/index.js
+++ backend-test/index.js
@@ -1,1 +1,14 @@
-console.log("hello suraj")
+import http from 'http'
+
+const message = 'Hello backend'
+
+const app = http.createServer((request, response) => {
+    response.writeHead(200, {'Content_Type': 'text/plain'})
+    response.end(message)
+})
+
+const PORT = 3001
+
+app.listen(PORT)
+
+console.log(`Server is running at port ${PORT}`)
