13e274ead4adccb415bf77669957a5c9ea5ae4bc
Suraj Mishra <mishrasur7@gmail.com>
2022-11-08 23:09:52 +0200

    port number changed to 3001

--- Part3/index.js
+++ Part3/index.js
@@ -29,7 +29,7 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-const PORT = 3000
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`server is up and running at port ${PORT}`)
 })
