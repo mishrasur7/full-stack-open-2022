10805a718b2f3edae735cafcb73a22e2d5825418
Suraj Mishra <mishrasur7@gmail.com>
2022-11-08 23:53:21 +0200

    part3 phonebook backend step2 completed

--- Part3/index.js
+++ Part3/index.js
@@ -20,13 +20,33 @@ const persons = [
       "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
-    }
+    },
+    { 
+        "id": 5,
+        "name": "suraj Poppendieck", 
+        "number": "39-23-6423122"
+      }
 ]
 
 const app = express()
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+const getTotalPersons = () => {
+    let total = 0
+    persons.map(person => total += 1)
+    return total
+}
+
+const getRequestTime = () => {
+    const date = new Date().toString()
+    return date
+}
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phone book has info for ${getTotalPersons()} people <br /> ${getRequestTime()} </p>`)
 })
 
 const PORT = 3001
