5552de95d1d7415325c7c393807987fc1bc690de
Suraj Mishra <mishrasur7@gmail.com>
2022-11-10 00:56:55 +0200

    note app deployed as full stack but data in momory to Fly.io

--- practice_backend/index.js
+++ practice_backend/index.js
@@ -33,6 +33,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 app.get('/', (request, response) => {
     response.send('Hello suraj')
@@ -110,7 +111,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+// const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || "8080";
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
