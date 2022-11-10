9de31ee333d2105065f6df217a58fcebb2a66371
Suraj Mishra <mishrasur7@gmail.com>
2022-11-10 13:51:35 +0200

    part3 phonebook backend step9 completed -baseul modified in Part2/src/services/persons.js and cors installed in Part3 and used as middleware in Part3/index.js

--- Part3/index.js
+++ Part3/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
@@ -28,7 +29,13 @@ let persons = [
         "id": 5,
         "name": "suraj Poppendieck", 
         "number": "39-23-6423122"
-      }
+      }, 
+      { 
+        "id": 6,
+        "name": "Shova Poppendieck", 
+        "number": "39-23-6423122"
+      },
+
 ]
 
 //useful methods and variables
@@ -54,6 +61,7 @@ const maxId = persons.length > 0
 
 //global middlewares
 app.use(express.json())
+app.use(cors())
 
 // app.use(morgan('tiny'))
 
