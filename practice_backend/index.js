c91f05bc7780642bf664eefaaef961bafd0eb9ed
Suraj Mishra <mishrasur7@gmail.com>
2022-11-13 22:34:17 +0200

    model separated, dotenv installed, and model object used for data handling

--- practice_backend/index.js
+++ practice_backend/index.js
@@ -1,6 +1,10 @@
-import http, { request } from 'http'
+import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
+
+import Note from './models/note.js'
+
+dotenv.config()
 
 let notes = [
     {
@@ -40,35 +44,29 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request,response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      return response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
+    const id = request.params.id
     console.log(id)
-    const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-        return note.id === id
-    })
-
-    note
-    ? response.json(note)
-    : (response.statusMessage = `Note with given id ${id} not!`, response.status(404).end())
+    const note = Note.findById(request.params.id).then(note => response.json(note))
   })
 
   app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+    const id = request.params.id
+    notes = Note.filter(note => note.id !== id)
     response.status(204).end()
-
   })
 
-  const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(note => note.id))
-        : 0
-    return maxId + 1
-  }
+  // const generateId = () => {
+  //   const maxId = notes.length > 0
+  //       ? Math.max(...notes.map(note => note.id))
+  //       : 0
+  //   return maxId + 1
+  // }
 
   app.post('/api/notes', (request, response) => {
     console.log(request.headers)
@@ -78,16 +76,13 @@ app.get('/api/notes/:id', (request, response) => {
         return response.status(400).json({error: "content missing"})
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId()
-    }
+        date: new Date()
+    })
 
-    console.log(note)
-    notes = notes.concat(note)
-    response.json(note)
+    note.save().then(savedNote => response.json(savedNote))
   })
 
 // const app = http.createServer((request, response) => {
@@ -111,7 +106,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-// const PORT = process.env.PORT || 3001
+
 const PORT = process.env.PORT || "8080";
 
 app.listen(PORT, () => {
