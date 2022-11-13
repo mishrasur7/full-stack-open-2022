db68f48a85c0cfa5514bf0693d5e7320a8c48941
Suraj Mishra <mishrasur7@gmail.com>
2022-11-13 11:58:15 +0200

    condition added and required output printed to console

--- Part3/mongo.js
+++ Part3/mongo.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
-if (process.argv.length < 3) {
+if (process.argv.length > 3 && process.argv.length < 5) {
     console.log('Please provide the password, name and phone number as an argument: node mongo.js <password> <name> <phnumber>')
+    process.exit(1)
+} else if(process.argv.length < 3) {
+    console.log('Please provide password as an argument: node mongo.js <password>')
     process.exit(1)
 }
 
@@ -18,28 +21,29 @@ const phoneSchema = new mongoose.Schema({
 
 const PhoneBook = mongoose.model('PhoneBook', phoneSchema)
 
-mongoose
-    .connect(url)
-    .then(result => {
-        console.log(result)
-        console.log('Connected to database')
-        const phoneBookInfo = new PhoneBook({
-            name: name,
-            number: phNumber
-        })
-        return phoneBookInfo.save()
-    })
-    .then(() => {
-        console.log(`added ${name} number ${phNumber} to phonebook`)
-        mongoose.connection.close()
-    })
-    .catch(err => console.log(err))
-
 // mongoose
 //     .connect(url)
-//     .then(() => PhoneBook.find({}))
 //     .then(result => {
-//         result.forEach(phoneInfo => console.log(phoneInfo))
+//         console.log(result)
+//         console.log('Connected to database')
+//         const phoneBookInfo = new PhoneBook({
+//             name: name,
+//             number: phNumber
+//         })
+//         return phoneBookInfo.save()
+//     })
+//     .then(() => {
+//         console.log(`added ${name} number ${phNumber} to phonebook`)
 //         mongoose.connection.close()
 //     })
 //     .catch(err => console.log(err))
+
+mongoose
+    .connect(url)
+    .then(() => PhoneBook.find({}))
+    .then(result => {
+        console.log('phonebook: ')
+        result.forEach(phoneInfo => console.log(`${phoneInfo.name} ${phoneInfo.number}`))
+        mongoose.connection.close()
+    })
+    .catch(err => console.log(err))
