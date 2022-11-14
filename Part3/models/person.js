163668261dc04968fed7f8e8bb35a53f797e4f58
Suraj Mishra <mishrasur7@gmail.com>
2022-11-15 01:14:15 +0200

    part3 3.22 lint configuration completed --hence the Part3 has now been completed totally

--- Part3/models/person.js
+++ Part3/models/person.js
@@ -3,40 +3,41 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URL
 
 console.log(`Connecting to ${url}`)
 
 mongoose
-    .connect(url)
-    .then(() => {console.log('Connected to database')})
-    .catch(err => console.log(`Error connecting to mongodb: ${err.message}`))
+  .connect(url)
+  .then(() => {console.log('Connected to database')})
+  .catch(err => console.log(`Error connecting to mongodb: ${err.message}`))
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{6,}/.test(v)
+      },
+      message: (props) => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return /\d{2,3}-\d{6,}/.test(v);
-            },
-            message: (props) => `${props.value} is not a valid phone number!`
-        },
-        required: [true, 'User phone number required!'],
-        minLength: 8
-    }
+    required: [true, 'User phone number required!'],
+    minLength: 8
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const Person = mongoose.model('phoneBook', personSchema)
