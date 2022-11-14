b4edf7710c4514cd96fd54197912b80a98c4629f
Suraj Mishra <mishrasur7@gmail.com>
2022-11-14 12:58:02 +0200

    code updated

--- practice_backend/index.js
+++ practice_backend/index.js
@@ -49,7 +49,7 @@ app.get('/api/notes', (request,response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note
       .findById(request.params.id)
       .then(note => {
@@ -57,15 +57,14 @@ app.get('/api/notes/:id', (request, response) => {
         ? response.json(note)
         : response.status(404).end()
       })
-      .catch(err => {
-        console.log(`Internal server error: ${err.message}`)
-        response.status(400).send({error: 'malformatted id'})
+      .catch(error => {
+        next(error)
       })
   })
 
-  app.delete('/api/notes/:id', (request, response, next) => {
-    const id = request.params.id
-    // notes = Note.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  // notes = Note.filter(note => note.id !== id)
     Note
       .findByIdAndDelete(id)
       .then(response.status(204).end())
@@ -79,22 +78,43 @@ app.get('/api/notes/:id', (request, response) => {
   //   return maxId + 1
   // }
 
-  app.post('/api/notes', (request, response) => {
-    console.log(request.headers)
-    const body = request.body
+app.post('/api/notes', (request, response) => {
+  console.log(request.headers)
+  const body = request.body
 
-    if(!body.content){
-        return response.status(400).json({error: "content missing"})
-    }
+  if(!body.content){
+      return response.status(400).json({error: "content missing"})
+  }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date()
-    })
-
-    note.save().then(savedNote => response.json(savedNote))
+  const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date()
   })
+
+  note.save().then(savedNote => response.json(savedNote))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note
+    .findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      console.log(note)
+      console.log(updatedNote)
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
+
 
 // const app = http.createServer((request, response) => {
 //     response.writeHead(200, { 'Content-Type': 'text/plain' })
@@ -116,6 +136,16 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, response, request, next) => {
+  console.log(error.name)
+  if(error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || "8080";
