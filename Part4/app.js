839b729dc8d22f22af7d86c770a2ff3ff5131ff8
Suraj Mishra <mishrasur7@gmail.com>
2022-11-16 10:30:14 +0200

    Part4 added

---
+++ Part4/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import config from './utils/config.js'
+import blogsRouter from './controllers/blogs.js'
+import middware from './utils/middleware.js'
+import logger from './utils/logger.js'
+
+const app = express()
+
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => logger.info('Connected to database'))
+    .catch((error) => logger.error('Error connecting to database', error.message))
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middware.unknownEndPoint)
+app.use(middware.errorHandler)
+
+export default app
