d8cc8709cec24cc3cdaea4a78280c2ae16555f80
Suraj Mishra <mishrasur7@gmail.com>
2022-11-17 12:03:42 +0200

    part4 helper functions and unit tests step2 completd

--- Part4/tests/list_helper.test.js
+++ Part4/tests/list_helper.test.js
@@ -8,3 +8,29 @@ describe('dummy', () => {
     expect(result).toBe(1)
   })
 })
+
+describe('totalLikes', () => {
+  test('totalLikes returns', () => {
+    const blogs = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 10,
+        __v: 0
+      }
+    ]
+    const totalLikes = list_helper.totalLikes(blogs)
+    console.log('totallikes', totalLikes)
+    expect(totalLikes).toBe(15)
+  })
+})
--- Part4/utils/list_helper.js
+++ Part4/utils/list_helper.js
@@ -1,8 +1,18 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 const dummy = (array) => {
   return 1
 }
 
+const totalLikes = (list) => {
+  const reducer = (sum, item) => {
+    return sum.likes + item.likes
+  }
+  return list.length === 0
+    ? 0
+    : list.reduce(reducer)
+}
+
 export default {
-  dummy
+  dummy,
+  totalLikes
 }
