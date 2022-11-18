da5b4836a7e5334ce9b85c60f8019ffad7228763
Suraj Mishra <mishrasur7@gmail.com>
2022-11-18 16:37:56 +0200

    database cleared before each test

--- practice_backend/tests/note_api.test.js
+++ practice_backend/tests/note_api.test.js
@@ -1,8 +1,32 @@
 import mongoose from 'mongoose'
 import supertest from 'supertest'
+
 import app from '../app'
+import Note from '../models/note'
 
 const api = supertest(app)
+
+const initialNotes = [
+    {
+      content: 'HTML is easy',
+      date: new Date(),
+      important: false,
+    },
+    {
+      content: 'Browser can execute only Javascript',
+      date: new Date(),
+      important: true,
+    },
+  ]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
 describe('api testing', () => {
     test('notes are returned as json', async () => {
         await api
@@ -11,16 +35,18 @@ describe('api testing', () => {
           .expect('Content-Type', /application\/json/)
       }, 100000)
 
-      test('there are no notes', async () => {
+      test('all notes are returned', async () => {
         const response = await api.get('/api/notes')
       
-        expect(response.body).toHaveLength(11)
+        expect(response.body).toHaveLength(initialNotes.length)
       })
       
-      test('the first note is about HTTP methods', async () => {
+      test('a specific note is within the returned notes', async () => {
         const response = await api.get('/api/notes')
+
+        const contents = response.body.map(item => item.content)
       
-        expect(response.body[0].content).toBe('HTML is very very very difficult')
+        expect(contents).toContain('HTML is easy')
       })
 })
 
