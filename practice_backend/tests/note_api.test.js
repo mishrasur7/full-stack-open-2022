dda1c543862a677458957afd01d733ab4fbac914
Suraj Mishra <mishrasur7@gmail.com>
2022-11-18 12:32:48 +0200

    databse changed for testing and more test case added

--- practice_backend/tests/note_api.test.js
+++ practice_backend/tests/note_api.test.js
@@ -9,6 +9,18 @@ describe('api testing', () => {
           .get('/api/notes')
           .expect(200)
           .expect('Content-Type', /application\/json/)
+      }, 100000)
+
+      test('there are no notes', async () => {
+        const response = await api.get('/api/notes')
+      
+        expect(response.body).toHaveLength(11)
+      })
+      
+      test('the first note is about HTTP methods', async () => {
+        const response = await api.get('/api/notes')
+      
+        expect(response.body[0].content).toBe('HTML is very very very difficult')
       })
 })
 
