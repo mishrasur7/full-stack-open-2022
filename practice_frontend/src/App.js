ef116954ad86c1363f72f9cbc60b4b73c67aea7f
Suraj Mishra <mishrasur7@gmail.com>
2022-11-23 10:49:39 +0200

    login service added into practice frontend

--- practice_frontend/src/App.js
+++ practice_frontend/src/App.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
 
 import Note from './components/Note'
-import noteService from './services/notes'
 import './index.css'
 import Notification from './components/Notification'
+import noteService from './services/notes'
+import loginService from './services/login'
 
 const Footer = () => {
   const footerStyle = {
@@ -24,6 +25,9 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -76,11 +80,73 @@ const App = () => {
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
+  
+    const handleLogin = async (event) => {
+      event.preventDefault()
+      
+      try {
+        const user = await loginService.login({
+          username, password,
+        })
+        console.log('user from handlelogin: ', user)
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      } catch (exception) {
+        setErrorMessage('Wrong credentials')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+    }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>  
+  )
 
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+      {/* {user === null && loginForm()}
+      {user !== null && noteForm()} */}
+      {user === null
+       ? loginForm() 
+       : <div>
+        <p>logged is as {user.name}</p>
+        {noteForm()}
+       </div>
+      }
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -95,13 +161,6 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   )
