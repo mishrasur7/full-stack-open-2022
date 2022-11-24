e0cb2f20f56129d73fd1923ff7ae27254a23ee7e
Suraj Mishra <mishrasur7@gmail.com>
2022-11-24 23:47:32 +0200

    code updated

--- Part5/bloglist-frontend/src/App.js
+++ Part5/bloglist-frontend/src/App.js
@@ -70,7 +70,11 @@ const App = () => {
       }
       <br />
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} user={user} />
+        <Blog 
+          key={blog.id} 
+          blog={blog} 
+          user={user} 
+        />
       )}
     </div>
   )
