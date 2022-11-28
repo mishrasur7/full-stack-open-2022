b5c7df28a98a055b6cd4f06216c808060ba2a5ba
Suraj Mishra <mishrasur7@gmail.com>
2022-11-28 09:42:59 +0200

    part5 bloglist frontend step7 completed

--- Part5/bloglist-frontend/src/components/Blog.js
+++ Part5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,35 @@
+import { useState } from "react"
+
 const Blog = ({blog, user}) =>  {
+  const [detail, setDetail] = useState(true)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetail = () => {
+    setDetail(!detail)
+  }
+
   if(user !== null) {
     return (
       <>
-        <div>
-          {blog.title} {blog.author}
-        </div>
+          {detail ?
+            <div style={blogStyle}>
+              {blog.title}
+              <button onClick={toggleDetail}>view</button>
+            </div> :
+            <div style={blogStyle}>
+              {blog.title} <button onClick={toggleDetail}>hide</button> <br />
+              {blog.url} <br />
+              Likes 0 <button>like</button> <br />
+              {blog.author}
+            </div>
+          }
       </>
     )
   }
