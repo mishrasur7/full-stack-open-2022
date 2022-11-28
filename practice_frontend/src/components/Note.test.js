a86a5d7d0223b572430cf751b1f4603cec0f2c77
Suraj Mishra <mishrasur7@gmail.com>
2022-11-29 00:20:05 +0200

    user-event installed and mocked the toggleImportance function

--- practice_frontend/src/components/Note.test.js
+++ practice_frontend/src/components/Note.test.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
-test('renders content', () => {
-  const note = {
-    content: 'Component testing is done with react-testing-library',
-    important: true
-  }
+const note = {
+  content: 'Component testing is done with react-testing-library',
+  important: true
+}
 
+test('renders content', () => {
   //   render(<Note note={note} />)
   //   const element = screen.getByText('Component testing is done with react-testing-library')
   //   expect(element).toBeDefined()
@@ -16,5 +17,19 @@ test('renders content', () => {
   const { container } = render(<Note note={note}/>)
   const div = container.querySelector('.note')
   expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+
+  screen.debug()
+  screen.debug(div)
+})
+
+test('clickling the button calls event handler once', async () => {
+  const mockHandler = jest.fn()
+  render(<Note note={note} toggleImportance={mockHandler}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+
+  await user.click(button)
+  expect(mockHandler.mock.calls).toHaveLength(1)
 
 })
