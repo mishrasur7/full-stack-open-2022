b3e78396ebdeff2310cf47e52483a8c7a310ba82
Suraj Mishra <mishrasur7@gmail.com>
2022-11-29 11:03:40 +0200

    togglable component tested

--- practice_frontend/src/tests/Note.test.js
+++ practice_frontend/src/tests/Note.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import Note from './Note'
+import Note from '../components/Note'
 
 const note = {
   content: 'Component testing is done with react-testing-library',
