d3c068be5b36771c0d1b456cfff8664bc8e0bf31
Suraj Mishra <mishrasur7@gmail.com>
2022-12-02 08:33:47 +0200

    login with wrong credentials tested

--- practice_frontend/cypress/e2e/note_app.cy.js
+++ practice_frontend/cypress/e2e/note_app.cy.js
@@ -16,11 +16,45 @@ describe('Note app', function() {
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
   })
 
-  it('login form can be opened', function() {
+  it.only('login fails with wrong password', function() {
     cy.contains('login').click()
-    cy.get('#username').type('suraj')
-    cy.get('#password').type('mishra')
+    cy.get('#username').type('sahas')
+    cy.get('#password').type('wrong')
     cy.get('#login-button').click()
+
+    cy.contains('Wrong credentials')
   })
 
+  //   it('login form can be opened', function() {
+  //     cy.contains('login').click()
+  //     cy.get('#username').type('sahas')
+  //     cy.get('#password').type('mishra')
+  //     cy.get('#login-button').click()
+  //   })
+
+  //   describe('when logged in', function () {
+  //     beforeEach(function() {
+  //       cy.contains('login').click()
+  //       cy.get('#username').type('sahas')
+  //       cy.get('#password').type('mishra')
+  //       cy.get('#login-button').click()
+  //     })
+
+  //     describe('and a note exists', function () {
+  //       beforeEach(function () {
+  //         cy.contains('create note').click()
+  //         cy.get('input').type('another note cypress')
+  //         cy.contains('save').click()
+  //       })
+
+  //       it('it can be made important', function () {
+  //         cy.contains('another note cypress')
+  //           .contains('make important')
+  //           .click()
+
+//         cy.contains('another note cypress')
+//           .contains('make not important')
+//       })
+//     })
+//  })
 })
