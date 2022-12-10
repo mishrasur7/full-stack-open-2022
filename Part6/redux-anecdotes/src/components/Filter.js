1266528b9b02bc9c05147197daa1ef5bf0fa678d
Suraj Mishra <mishrasur7@gmail.com>
2022-12-10 23:55:19 +0200

    anecdotes and connect, step2 completed

--- Part6/redux-anecdotes/src/components/Filter.js
+++ Part6/redux-anecdotes/src/components/Filter.js
@@ -1,13 +1,15 @@
-import { useDispatch } from 'react-redux'
+//import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 
 import { setFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-    const dispatch = useDispatch()
+const Filter = (props) => {
+    //const dispatch = useDispatch()
 
     const handleChange = (event) => {
       const filterInput = event.target.value
-      dispatch(setFilter(filterInput)) 
+      //dispatch(setFilter(filterInput)) 
+      props.setFilter(filterInput)
     }
 
     const style = {
@@ -20,5 +22,9 @@ const Filter = () => {
       </div>
     )
   }
+
+  const mapDispatchToProps = {
+    setFilter
+  }
   
-  export default Filter
+  export default connect(null, mapDispatchToProps)(Filter)
