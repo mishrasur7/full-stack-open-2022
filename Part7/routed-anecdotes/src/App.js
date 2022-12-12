9b97984fc95c3dfb12417fcfb6649210aad9554d
Suraj Mishra <mishrasur7@gmail.com>
2022-12-12 23:57:33 +0200

    part7 anecdotes and hooks step1 completed

--- Part7/routed-anecdotes/src/App.js
+++ Part7/routed-anecdotes/src/App.js
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom'
 import { useParams, useNavigate } from 'react-router-dom'
 
+import { useField } from './hooks'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -52,18 +54,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -75,15 +77,30 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input
+            {...content}
+            // type={content.type} 
+            // value={content.value} 
+            // onChange={content.onChange} 
+          />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input 
+            {...author}
+            // type={author.type} 
+            // value={author.value} 
+            // onChange={author.onChange} 
+          />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input
+            {...info}
+            // type={info.type} 
+            // value={info.value} 
+            // onChange={info.onChange} 
+          />
         </div>
         <button>create</button>
       </form>
