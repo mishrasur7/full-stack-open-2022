c8698b91849f077d98804ee481428a3fb61db656
Suraj Mishra <mishrasur7@gmail.com>
2022-12-16 22:30:03 +0200

    devtool configured to source-map

--- practice_webpack/src/App.js
+++ practice_webpack/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react' // we need this now also in component files
 import './index.css'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState()
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
 
   return (
     <div className="container">
       hello webpack {counter} clicks
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={handleClick}>
         press
       </button>
     </div>
