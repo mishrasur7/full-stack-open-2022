03f3d5662f4bc99b8602ba0f7119cb08f058281e
Suraj Mishra <mishrasur7@gmail.com>
2022-12-17 07:04:06 +0200

    class component made with states and lifecycle methods

--- practice_class_components/src/App.js
+++ practice_class_components/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
@@ -10,18 +11,29 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount = () => {
+    axios.get('http://localhost:3001/anecdotes').then(response => {
+      this.setState({ anecdotes: response.data })
+    })
+  }
+
+  handleClick = () => {
+    const current = Math.floor(
+      Math.random() * this.state.anecdotes.length
+    )
+    this.setState({ current })
+  }
+
   render() {
-    if (this.state.anecdotes.length === 0) {
+    if (this.state.anecdotes.length === 0 ) {
       return <div>no anecdotes...</div>
     }
 
     return (
       <div>
         <h1>anecdote of the day</h1>
-        <div>
-          {this.state.anecdotes[this.state.current].content}
-        </div>
-        <button>next</button>
+        <div>{this.state.anecdotes[this.state.current].content}</div>
+        <button onClick={this.handleClick}>next</button>
       </div>
     )
   }
