d0e18cffce269d08fa9b02f51c6a40ee8648924d
Suraj Mishra <mishrasur7@gmail.com>
2022-12-22 23:35:49 +0200

    mutation added and prettier intalled/formatted codes

--- practice_graphql/index.js
+++ practice_graphql/index.js
@@ -1,30 +1,30 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
 let persons = [
-    {
-      name: "Arto Hellas",
-      phone: "040-123543",
-      street: "Tapiolankatu 5 A",
-      city: "Espoo",
-      id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-    },
-    {
-      name: "Matti Luukkainen",
-      phone: "040-432342",
-      street: "Malminkaari 10 A",
-      city: "Helsinki",
-      id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-    },
-    {
-      name: "Venla Ruuska",
-      street: "Nallemäentie 22 C",
-      city: "Helsinki",
-      id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-    },
-  ]
+  {
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki",
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki",
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
+  },
+];
 
-  const typeDefs = `#graphql
+const typeDefs = `#graphql
   type Address {
     street: String!
     city: String!
@@ -40,31 +40,45 @@ let persons = [
     allPersons: [Person!]!
     findPerson(name: String!): Person
   }
-`
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+  }
+`;
 const resolvers = {
-    Query: {
+  Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
-    Person: {
-        address: ({ street, city }) => {
-        return {
-            street,
-            city,
-        }
-        },
+  Person: {
+    address: ({ street, city }) => {
+      return {
+        street,
+        city,
+      };
     },
-  }
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      const person = { ...args, id: uuid() };
+      persons = persons.concat(person);
+      return person;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-})
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
-
