564f57d0f0fa94ca383a47aba27a653b0d405bbf
Suraj Mishra <mishrasur7@gmail.com>
2022-12-23 22:02:53 +0200

    part8 all books completed

--- Part8/index.js
+++ Part8/index.js
@@ -80,16 +80,25 @@ let authors = [
   ]
   
   const typeDefs = `#graphql
+    type Book {
+        title: String!
+        published: Int!
+        author: String!
+        genres: [String]!
+        id: ID!
+    }
     type Query {
         bookCount: Int!
         authorCount: Int!
+        allBooks: [Book!]!
     }
   `
   
   const resolvers = {
     Query: {
         bookCount: () => books.length,
-        authorCount: () => authors.length
+        authorCount: () => authors.length,
+        allBooks: () => books
     }
   }
   
