f27be0acb7d144814dc020e29417f31e140b3ccf
Suraj Mishra <mishrasur7@gmail.com>
2022-12-29 23:49:53 +0200

    part8 user and logging in completed

--- Part8/index.js
+++ Part8/index.js
@@ -3,9 +3,11 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { GraphQLError } from 'graphql';
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
 
 import Book from "./models/Book.js";
 import Author from "./models/Author.js";
+import User from "./models/User.js";
 
 dotenv.config()
 
@@ -88,6 +90,7 @@ let books = [
 ];
 
 const MONGODB_URI = process.env.MONGODB_URI
+const JWT_SECRET = process.env.JWT_SECRET
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
@@ -110,12 +113,23 @@ const typeDefs = `#graphql
         born: Int
         bookCount: Int!
     }
+
+    type User {
+      username: String!
+      favrouriteGenre: String!
+      id: ID!
+    }
+
+    type Token {
+      value: String!
+    }
     
     type Query {
         bookCount: Int!
         authorCount: Int!
         allBooks(author: String, genres: String): [Book!]!
         allAuthors: [Author!]!
+        me: User
     }
 
     type Mutation {
@@ -125,7 +139,22 @@ const typeDefs = `#graphql
             author: String!
             genres: [String]!
         ): Book
-        editAuthor(name: String!, setBornTo: Int!): Author
+
+        editAuthor(
+          name: String!, 
+          setBornTo: Int!
+          ): Author
+
+        createUser(
+          username: String!
+          favrouriteGenre: String!
+        ): User
+
+        login(
+          username: String!
+          password: String!
+        ): Token
+
     }
   `;
 
@@ -148,7 +177,10 @@ const resolvers = {
       //   }
       // });
     },
-    allAuthors: async () => await Author.find({})
+    allAuthors: async () => await Author.find({}),
+    me: (root, args, context) => {
+      return context.currentUser
+    }
   },
 
   Author: {
@@ -159,7 +191,13 @@ const resolvers = {
   },
 
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      const currentUser = context.currentUser
+
+      if(!currentUser) {
+        throw new GraphQLError('User not authenticated!')
+      }
+
       const author = await Author.findOne({name: args.author})
       
       if(!author) {
@@ -184,9 +222,15 @@ const resolvers = {
 
       return book
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+        const currentUser = context.currentUser
+
+        if(!currentUser) {
+          throw new GraphQLError('User not authenticated!')
+        }
+
         const author = await Author.findOne({name: args.name})
-        
+
         if(!author) {
             return null
         }
@@ -201,13 +245,49 @@ const resolvers = {
         
         const updated = await Author.findOne({born: args.setBornTo})
         return updated
-    } 
+    }, 
+    createUser: async (root, args) => {
+      console.log('args', args)
+      const newUser = new User({...args})
+      console.log(newUser)
+      try {
+        await newUser.save()
+      } catch (error) {
+        throw new GraphQLError(error.message)
+      }
+
+      return newUser
+    }, 
+    login: async (root, args) => {
+      const user = await User.findOne({username: args.username})
+
+      if ( !user || args.password !== 'mypassword' ) {
+        throw new GraphQLError("Wrong credentials!")
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    }
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const authorization = req ? req.headers.authorization : null
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        authorization.substring(7), JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 });
 
 const { url } = await startStandaloneServer(server, {
