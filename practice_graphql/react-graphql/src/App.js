2ad3e62e3db62369480251ed5071256008879164
Suraj Mishra <mishrasur7@gmail.com>
2022-12-30 11:40:41 +0200

    login added

--- practice_graphql/react-graphql/src/App.js
+++ practice_graphql/react-graphql/src/App.js
@@ -6,20 +6,36 @@ import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import Notify from "./components/Notify";
 import PhoneForm from "./components/PhoneForm";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState();
+  const [token, setToken] = useState(null)
 
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 1000,
   });
 
+  console.log('rest', result)
   const notify = (message) => {
     setErrorMessage(message);
     setTimeout(() => {
       setErrorMessage(null);
     }, 10000);
   };
+
+  if(!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
 
   if (result.loading) {
     return <div>loading...</div>;
