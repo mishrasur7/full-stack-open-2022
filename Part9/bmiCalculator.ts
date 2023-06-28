c0e993fb4ef6b9aa4761aff23f51dee5c8b63fd2
Suraj Mishra <mishrasur7@gmail.com>
2023-06-28 22:35:32 +0300

    part9 bmiCalculater done with command line arguments

--- Part9/bmiCalculator.ts
+++ Part9/bmiCalculator.ts
@@ -5,4 +5,32 @@ const calculateBmi = (height: number, weight: number) => {
     if(bmi > 80) return 'Overweight (unhealthy)'
 }
 
-console.log(calculateBmi(180, 74))
+interface ProvidedValues {
+    height: number,
+    weight: number
+}
+
+const parseArguments = (args: string[]): ProvidedValues => {
+    if(args.length < 4) throw new Error('Not enough arguments!');
+    if(args.length > 4) throw new Error('Too many argunents'); 
+
+    if(!isNaN(Number(args[3])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values are not numbers!')
+    }
+}
+
+try {
+    const {height, weight} = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch(error: unknown) {
+    let errorMessage = 'Something bad happened : ';
+    if(error instanceof Error) {
+        errorMessage += error.message
+    }
+    console.log(errorMessage)
+}
