eb7bf454fdc749b8addd02224bde50390a5bc68a
Suraj Mishra <mishrasur7@gmail.com>
2023-06-29 11:43:35 +0300

    Part9 exercise 9.5 completed

--- Part9/bmiCalculator.ts
+++ Part9/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number):any => {
     const heightInMeters = height/100
     const bmi = weight/(heightInMeters * heightInMeters)
     if(bmi < 80) return 'Normal (healthy weight)'
