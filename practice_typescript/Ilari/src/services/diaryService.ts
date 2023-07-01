b32833eb5e440b6483fe42952dc9fe8ec4662710
Suraj Mishra <mishrasur7@gmail.com>
2023-07-01 15:58:11 +0300

    route tested

---
+++ practice_typescript/Ilari/src/services/diaryService.ts
@@ -0,0 +1,25 @@
+import diaryEntries from '../data/entries';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+
+const getEntries = (): DiaryEntry[] => {
+  return diaryEntries;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaryEntries.map(({id, date, weather, visibility}) => ({
+    id,
+    date,
+    weather,
+    visibility
+  }));
+}
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+  getNonSensitiveEntries
+};
