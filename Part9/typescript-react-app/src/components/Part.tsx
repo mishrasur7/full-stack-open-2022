d9a65dba6e574120794e1c044c7cf2221e8d61cc
Suraj Mishra <mishrasur7@gmail.com>
2023-07-04 16:45:43 +0300

    code refractored for 9.15 exercise

--- Part9/typescript-react-app/src/components/Part.tsx
+++ Part9/typescript-react-app/src/components/Part.tsx
@@ -8,15 +8,28 @@ const Part = (props: Parts) => {
         props.courses.map((course) => {
             switch(course.kind) {
                 case "basic":
-                    return <div><h3>{course.name} {course.exerciseCount}</h3> <p>{course.description}</p></div>
+                    return <div>
+                            <h3>{course.name} {course.exerciseCount}</h3>
+                            <p>{course.description}</p>
+                        </div>
                     break;  
                 case "group":
-                    return <div><h3>{course.name} {course.exerciseCount}</h3> <p>Project exercises {course.groupProjectCount}</p></div>; 
+                    return <div>
+                            <h3>{course.name} {course.exerciseCount}</h3> 
+                            <p>Project exercises {course.groupProjectCount}</p>
+                        </div>; 
                     break;
                 case "background": 
-                    return <div><h3>{course.name} {course.exerciseCount}</h3> <p>{course.description}</p><p>Submit to {course.backgroundMaterial}</p></div>
+                    return <div>
+                            <h3>{course.name} {course.exerciseCount}</h3> 
+                            <p>{course.description}</p><p>Submit to {course.backgroundMaterial}</p>
+                        </div>
                 case "special":
-                    return <div><h3>{course.name} {course.exerciseCount}</h3> <p>{course.description} </p><p>Required skills {course.requirements.map(r => `${r}, `)}</p></div>
+                    return <div>
+                            <h3>{course.name} {course.exerciseCount}</h3> 
+                            <p>{course.description} </p>
+                            <p>Required skills {course.requirements.map(r => `${r}, `)}</p>
+                        </div>
                 default:
                     assertNever(course);
             }
