1bd7548580584c68ed50a796596a33bf5f7089de
Suraj Mishra <mishrasur7@gmail.com>
2023-07-10 23:34:01 +0300

    eslint error fixed

--- practice_typescript/multiplier.ts
+++ practice_typescript/multiplier.ts
@@ -1,6 +1,6 @@
 const multiplicator = (a:number, b:number, printText:string) => {
     console.log(printText, a * b); 
-}
+};
 
 interface MultiplyValues {
     value1: number,
@@ -15,19 +15,19 @@ const parseArguments = (args: string[]): MultiplyValues => {
         return {
             value1: Number(args[2]),
             value2: Number(args[3])
-        }
+        };
     } else {
-        throw new Error('Provided values were not numbers')
+        throw new Error('Provided values were not numbers');
     }
-}
+};
 
 try {
     const {value1, value2} = parseArguments(process.argv);
     multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is: `);
 } catch (error: unknown) {
-    let errorMessage = 'Something bad happened -> '
+    let errorMessage = 'Something bad happened -> ';
     if(error instanceof Error) {
-        errorMessage += 'Error:' + error.message
+        errorMessage += 'Error:' + error.message;
     }
-    console.log(errorMessage)
+    console.log(errorMessage);
 }
